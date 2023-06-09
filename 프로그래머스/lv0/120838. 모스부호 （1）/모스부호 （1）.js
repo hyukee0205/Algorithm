cc443c9208b59387a281dceb0493d87363d34baa
hyukee0205 <107531168+hyukee0205@users.noreply.github.com>
2023-06-09 23:59:36 +0900

    [level 0] Title: 모스부호 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,27 @@
+morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+function solution(letter) {
+    let currentMorse = '';
+    let answer = '';
+    for(let i=0; i<letter.length; i++) {
+        const item = letter[i];
+        
+        if(item === ' ') {
+            answer += morse[currentMorse];
+            currentMorse = '';
+        } else {
+            currentMorse += item;
+        }
+    } 
+    answer += morse[currentMorse];
+
+    
+    return answer;
+    
+}
