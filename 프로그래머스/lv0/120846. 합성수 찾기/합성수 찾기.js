c6a2553578edadba218994e69eac762ff159f445
hyukee0205 <107531168+hyukee0205@users.noreply.github.com>
2023-06-24 12:11:25 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    let answer = 0;
+    
+    for(let i = n; i >= 1; i--) {
+
+    let isCount = false;
+    for(let num = 2; num < i-1; num++) {
+        if(i % num === 0) {
+            isCount = true;
+        }
+        
+    }
+        if(isCount) {
+            answer = answer + 1;
+        }
+            
+    }
+    
+    
+    return answer;
+    
+    
+}
