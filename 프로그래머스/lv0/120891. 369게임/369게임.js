0ea574cde46c9f02029fdfe0a2c0e3a0ecba9067
hyukee0205 <107531168+hyukee0205@users.noreply.github.com>
2023-06-27 17:27:26 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,14 @@
+function solution(order) {
+    let answer = 0;
+    const stringOrder = String(order);
+    
+    for(let i=0; i < stringOrder.length; i++) {
+        const item = stringOrder[i];
+        if(item === '3' || item === '6' || item === '9') {
+            answer = answer + 1;
+        }
+    }
+    
+    
+    return answer;
+}
