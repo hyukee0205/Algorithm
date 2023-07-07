2131e689f96e50e27a853bd65899aa8b013cd41e
hyukee0205 <107531168+hyukee0205@users.noreply.github.com>
2023-07-07 17:22:43 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    
+    let answer = 0;
+    let m = String(n);
+    
+    for(let i = 0; i < m.length; i++) {
+        answer +=  parseInt(m[i]);
+    }
+    
+    return answer;
+    
+}
